ea831810156d6363bf9225aa1f752a5ef481cae1
Griffin Poole <griffinpoole5@gmail.com>
2019-10-11 18:53:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer)
 end
 
 def long_planeteer_calls(planeteer)
-  planeteer.find { |call| call.length >= 4}
+  !!(planeteer.find { |call| call.length >= 4})
 end
 
 def find_the_cheese# code an argument here
