f4a832465160c025dd061cc87abb0928aaa1b935
Griffin Poole <griffinpoole5@gmail.com>
2019-10-11 18:48:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteer)
   planeteer.map {|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer)
+  planeteer.reduce
 end
 
 def find_the_cheese# code an argument here
