aab7e70594ef1c88bbb11104932c03702e3261de
Griffin Poole <griffinpoole5@gmail.com>
2019-10-11 18:46:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer)
-  planeteer.map {|call| "#{call.}"}
+  planeteer.map {|call| "#{call.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
